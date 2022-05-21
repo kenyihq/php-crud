fd889881bec84ebca7c1e54fa99fcbd749c0b21d
Kenyi Hancco <kenyihq@gmail.com>
2022-05-21 00:03:33 -0500

    Added includes files, header and footer

--- Database/php-crud.sql
+++ Database/php-crud.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `php-crud`;
 USE `php-crud`;
 
+CREATE TABLE `php-crud`.`task` ( `id` INT NOT NULL AUTO_INCREMENT ,  `title` VARCHAR(255) NOT NULL ,  `description` TEXT NOT NULL ,  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;
