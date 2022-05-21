9db5a563d536c371d5ba3e63b712574a2a0fd789
Kenyi Hancco <kenyihq@gmail.com>
2022-05-21 11:15:39 -0500

    Create save_task code

--- save_task.php
+++ save_task.php
@@ -1,7 +1,19 @@
 <?php
 
+include("db.php");
+
 if (isset($_POST['save_task'])){
-    echo "Saving task";
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $query = "INSERT INTO task (title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result){
+        die("Query Failed");
+    }
+    
+    echo "Task Saved Successfully";
 }
 
 ?>
