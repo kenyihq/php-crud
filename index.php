86543279438dd8284b2c1381d1e09d912a69db46
Kenyi Hancco <kenyihq@gmail.com>
2022-05-21 14:11:09 -0500

    Added table that brings data from database

--- index.php
+++ index.php
@@ -29,8 +29,35 @@ include("db.php");
                     <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task">
                 </form>
 
-            </div>
+            </div>            
+        </div>
 
+        <div class="col-md-8">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Created At</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM task";
+                    $result_tasks = mysqli_query($conn, $query);
+
+                    while($row = mysqli_fetch_array($result_tasks)) { ?>
+                        <tr>
+                            <td><?php echo $row['title'] ?></td>
+                            <td><?php echo $row['description'] ?></td>
+                            <td><?php echo $row['created_at'] ?></td>
+                        </tr>
+                    
+
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
