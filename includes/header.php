0c40a1eafdebcdce0b82557ac5085a24857fd21c
Kenyi Hancco <kenyihq@gmail.com>
2022-05-21 00:19:48 -0500

    Created first desing

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task</title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">PHP CRUD</a>
+        </div>
+    </nav>
     
--- index.php
+++ index.php
@@ -4,7 +4,25 @@ include("db.php");
 
 <?php include("includes/header.php"); ?>
 
-<h1>Hola mundo</h1>
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-body">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="description" rows="2" class="form-control" placeholder="Task description"></textarea>
+                    </div>
+                    <input type="text" type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task">
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+</div>
 
 
 <?php include("includes/footer.php"); ?>
